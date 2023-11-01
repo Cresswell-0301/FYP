e57b54d1c61d162d0710c9c3dc512fc08b9ea9cd
Cresswell-0301 <129597429+Cresswell-0301@users.noreply.github.com>
2023-11-02 07:55:42 +0800

    navigation.js bar when small can click and when screen biger it display corectly

--- pages/navigation.js
+++ pages/navigation.js
@@ -1,4 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
 export default function Navigation (){
+    const [isBarHidden, setIsBarHidden] = useState(true);
+    const [isButtonVisible, setIsButtonVisible] = useState(true);
+
+    const toggleBar = () => {
+        setIsBarHidden((prevIsBarHidden) => !prevIsBarHidden);
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+        const mobileMenu = document.getElementById('menu-toggle-button');
+        const menuToggle = document.getElementById('mobile-menu-2');
+        if (window.innerWidth <= 768) {
+            setIsButtonVisible(true);
+            if (isButtonVisible) {
+            menuToggle.style.display = isBarHidden ? 'none' : 'block';
+            } else {
+            menuToggle.style.display = 'none';
+            }
+        } else {
+            setIsButtonVisible(false);
+            menuToggle.style.display = 'block';
+        }
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+    }, [isBarHidden, isButtonVisible]);
+
+    const mobileMenuStyle = {
+        display: isBarHidden ? 'none' : 'block',
+    };
+
     return(
         <div>
             <nav className="bg-white border-gray-200 py-2.5 dark:bg-gray-900">
@@ -19,23 +57,22 @@ export default function Navigation (){
                         </a>
 
                         <button
-                        data-collapse-toggle="mobile-menu-2"
                         type="button"
                         id="menu-toggle-button"
                         className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="mobile-menu-2"
-                        aria-expanded="false"
+                        aria-expanded={isBarHidden ? 'false' : 'true'}
+                        onClick={toggleBar}
                         >
 
                             <span className="sr-only">Open main menu</span>
                             
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <svg className={`w-6 h-6 ${isBarHidden ? '' : 'hidden'}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                                     clip-rule="evenodd"></path>
                             </svg>
                             
-                            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <svg className={`w-6 h-6 ${isBarHidden ? 'hidden' : ''}`} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
                                     d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                     clip-rule="evenodd"></path>
@@ -43,8 +80,8 @@ export default function Navigation (){
                         </button>
                     </div>
 
-                    <div className="items-center justify-between w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                    <div className="items-center justify-between w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2" style={mobileMenuStyle}>
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 text-center">
                             <li>
                                 <a href="#"
                                     className="block py-2 pl-3 pr-4 text-white bg-purple-700 rounded lg:bg-transparent lg:text-purple-700 lg:p-0 dark:text-white"
